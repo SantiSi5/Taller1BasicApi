1b4fb3c9b1dcc009a9c0f78fd6afd72e5a19f2f3
SANTIAGO SIERRA BALLESTEROS <santiagosierra1115433@correo.itm.edu.co>
2025-09-14 18:45:06 -0500

    Basic structure and database

---
+++ Taller 1/Taller.Shared/Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taller.Shared.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Nombre")]
+    [MaxLength(50, ErrorMessage = "El Campo {0} no puede tener mas de {1} carácteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Apellido")]
+    [MaxLength(50, ErrorMessage = "El Campo {0} no puede tener mas de {1} carácteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string LastName { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    [Display(Name = "Fecha de contratación")]
+    [DataType(DataType.Date)]
+    public DateTime HireDate { get; set; }
+
+    [Display(Name = "Salario")]
+    [Range(1000000, double.MaxValue, ErrorMessage = "El campo {0} debe ser mínimo {1}.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public decimal Salary { get; set; }
+}
